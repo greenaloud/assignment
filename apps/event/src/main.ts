c5f605445e5aafdd89062ca1af6b4031bd23b042
조원우 <greenaloud@gmail.com>
2025-05-15 20:39:34 +0900

    enable pino logger for event app

--- apps/event/src/main.ts
+++ apps/event/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { EventsModule } from './events.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(EventsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
