76ee878b27fe151dbd3d7b97cd6e12ed5ef0d20a
조원우 <greenaloud@gmail.com>
2025-05-15 20:39:19 +0900

    add validation pipe to event app

--- apps/event/src/main.ts
+++ apps/event/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { EventsModule } from './events.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(EventsModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
