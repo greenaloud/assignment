bd0b79338a90e38d50a10cdf6ceca7c9ff38df16
조원우 <greenaloud@gmail.com>
2025-05-16 23:47:54 +0900

    implement login feature

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { validationSchema } from './config/validation.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongooseConfig } from './config/mongoose.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { MongooseConfig } from './config/mongoose.config';
     MongooseModule.forRootAsync({ useClass: MongooseConfig }),
     LoggerModule,
     UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
---
+++ apps/auth/src/auth/auth.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtConfig } from '../config/jwt.config';
+
+@Module({
+  imports: [JwtModule.registerAsync({ useClass: JwtConfig }), UsersModule],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy],
+})
+export class AuthModule {}
