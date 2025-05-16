3c5994865192ea5e7a68b37c41e87d92a1f46eb3
조원우 <greenaloud@gmail.com>
2025-05-16 21:23:33 +0900

    get port from env

--- apps/auth/src/config/validation.schema.ts
+++ apps/auth/src/config/validation.schema.ts
@@ -1,6 +1,7 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
+  PORT: Joi.number().required(),
   MONGODB_URI: Joi.string().required(),
   JWT_SECRET: Joi.string().required(),
   JWT_EXPIRATION: Joi.string().required(),
--- apps/event/src/config/validation.schema.ts
+++ apps/event/src/config/validation.schema.ts
@@ -1,5 +1,6 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
+  PORT: Joi.number().required(),
   MONGODB_URI: Joi.string().required(),
 });
