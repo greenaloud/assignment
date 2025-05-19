0e920e2a9c12d00cbbc86153d1f2e8ba096288a3
조원우 <greenaloud@gmail.com>
2025-05-19 11:21:42 +0900

    add json middleware

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -5,6 +5,7 @@ import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
 import { Transport } from '@nestjs/microservices';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +19,7 @@ async function bootstrap() {
     },
   });
 
+  app.use(json());
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(json());
   app.use(cookieParser());
   app.useLogger(app.get(Logger));
 
